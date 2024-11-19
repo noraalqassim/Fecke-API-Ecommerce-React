6914cadce8a4d04ac6febaa8e43f45f456747e97
noraalqassim <noraalqassim12@gmail.com>
2024-11-19 22:54:18 +0300

    Fix the errors names

--- src/components/layoutt/Layout.js
+++ src/components/layoutt/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../navbar/NavBar";
+import NavBar from "../navbarr/NavBar";
 import Footer from "../foolder/Footer";
 import { Outlet } from "react-router-dom";
 
